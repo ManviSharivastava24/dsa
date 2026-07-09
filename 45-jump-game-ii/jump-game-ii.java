33f2250fdcbab2e987740b3c204666dc779d66f6
ManviSharivastava24 <manvishrivastava0516@gmail.com>
2026-07-09 18:20:12 +0530

    Time: 112 ms (8.32%) | Memory: 47.4 MB (21.68%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    int[] dp;
+
+    public int jump(int[] nums) {
+        int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return solve(0, nums);
+    }
+
+    int solve(int idx, int[] nums) {
+
+        if (idx >= nums.length - 1)
+    return 0;
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+      int ans = Integer.MAX_VALUE;
+
+for (int jump = 1; jump <= nums[idx]; jump++) {
+
+    int next = solve(idx + jump, nums);
+
+    if (next != Integer.MAX_VALUE) {
+        ans = Math.min(ans, 1 + next);
+    }
+}
+
+return dp[idx] = ans;
+
+  
+    }
+}
