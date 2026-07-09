703c3593be2592d8e5fbbadcce1ac560496475c6
ManviSharivastava24 <manvishrivastava0516@gmail.com>
2026-07-09 18:37:12 +0530

    Time: 131 ms (5.23%) | Memory: 47.5 MB (8.65%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -3,8 +3,7 @@ class Solution {
     int[] dp;
 
     public int jump(int[] nums) {
-        int n = nums.length;
-        dp = new int[n];
+        dp = new int[nums.length];
         Arrays.fill(dp, -1);
         return solve(0, nums);
     }
@@ -12,24 +11,24 @@ class Solution {
     int solve(int idx, int[] nums) {
 
         if (idx >= nums.length - 1)
-    return 0;
+            return 0;
 
         if (dp[idx] != -1)
             return dp[idx];
 
-      int ans = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
 
-for (int jump = 1; jump <= nums[idx]; jump++) {
+        for (int jump = 1;
+             jump <= nums[idx] && idx + jump < nums.length;
+             jump++) {
 
-    int next = solve(idx + jump, nums);
+            int next = solve(idx + jump, nums);
 
-    if (next != Integer.MAX_VALUE) {
-        ans = Math.min(ans, 1 + next);
-    }
-}
+            if (next != Integer.MAX_VALUE) {
+                ans = Math.min(ans, 1 + next);
+            }
+        }
 
-return dp[idx] = ans;
-
-  
+        return dp[idx] = ans;
     }
 }
