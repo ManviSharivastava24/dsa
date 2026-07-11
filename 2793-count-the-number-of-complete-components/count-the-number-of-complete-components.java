89767584617ffa5f8982d2b868391b943a39f1aa
ManviSharivastava24 <manvishrivastava0516@gmail.com>
2026-07-11 11:40:32 +0530

    Time: 9 ms (82.85%) | Memory: 47.8 MB (39.49%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,55 +1,51 @@
 class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
 
-    ArrayList<Integer>[] graph;
-    boolean[] vis;
-
-    int nodes;
-    int edges;
-
-    public int countCompleteComponents(int n, int[][] edgesArr) {
-
-        graph = new ArrayList[n];
-        vis = new boolean[n];
-
+        ArrayList<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++)
             graph[i] = new ArrayList<>();
 
-        for (int[] e : edgesArr) {
+        for (int[] e : edges) {
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
 
+        boolean[] vis = new boolean[n];
         int ans = 0;
 
         for (int i = 0; i < n; i++) {
 
-            if (!vis[i]) {
+            if (vis[i])
+                continue;
 
-                nodes = 0;
-                edges = 0;
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(i);
+            vis[i] = true;
 
-                dfs(i);
+            int nodes = 0;
+            int degreeSum = 0;
 
-                edges /= 2;
+            while (!q.isEmpty()) {
 
-                if (edges == nodes * (nodes - 1) / 2)
-                    ans++;
+                int curr = q.poll();
+                nodes++;
+
+                degreeSum += graph[curr].size();
+
+                for (int nei : graph[curr]) {
+                    if (!vis[nei]) {
+                        vis[nei] = true;
+                        q.offer(nei);
+                    }
+                }
             }
+
+            int edgeCount = degreeSum / 2;
+
+            if (edgeCount == nodes * (nodes - 1) / 2)
+                ans++;
         }
 
         return ans;
-    }
-
-    void dfs(int u) {
-
-        vis[u] = true;
-        nodes++;
-
-        edges += graph[u].size();
-
-        for (int v : graph[u]) {
-            if (!vis[v])
-                dfs(v);
-        }
     }
 }
