6a57c28b470d8ec50480b8baab88bf5d40985e65
ManviSharivastava24 <manvishrivastava0516@gmail.com>
2026-07-17 23:01:16 +0530

    Time: 3 ms (87.97%) | Memory: 52 MB (75.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,50 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1') {
-
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'&&vis[i][j]==0){
                     count++;
-
-                    Queue<int[]> q = new LinkedList<>();
-                    q.offer(new int[]{i, j});
-                    grid[i][j] = '0';
-
-                    int[] dx = {-1, 0, 1, 0};
-                    int[] dy = {0, 1, 0, -1};
-
-                    while (!q.isEmpty()) {
-
-                        int[] cur = q.poll();
-
-                        int x = cur[0];
-                        int y = cur[1];
-
-                        for (int k = 0; k < 4; k++) {
-
-                            int nx = x + dx[k];
-                            int ny = y + dy[k];
-
-                            if (nx >= 0 && nx < n &&
-                                ny >= 0 && ny < m &&
-                                grid[nx][ny] == '1') {
-
-                                grid[nx][ny] = '0';
-                                q.offer(new int[]{nx, ny});
-                            }
-                        }
-                    }
+                    dfs(i,j,grid,vis);
                 }
             }
         }
-
-        return count;
+return count;
     }
+     public void dfs(int i,int j,char[][] grid, int vis[][]){
+         int n=grid.length;
+        int m=grid[0].length;
+    if(i<0||j<0||i>=n||j>=m||grid[i][j]=='0'){
+        return ;
+    }
+    vis[i][j]=1;
+    grid[i][j]='0';
+    dfs(i+1,j,grid,vis);
+    dfs(i,j+1,grid,vis);
+    dfs(i-1,j,grid,vis);
+    dfs(i,j-1,grid,vis);
+ }
 }
